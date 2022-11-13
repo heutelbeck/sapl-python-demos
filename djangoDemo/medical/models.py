3622dc014daf842ae0b84b66b70c48c7500784c4
Dominik Wuttke <Dominikwuttke@web.de>
2022-11-14 00:37:01 +0100

    - refactor Patient Model rename fields

--- djangoDemo/medical/models.py
+++ djangoDemo/medical/models.py
@@ -1,35 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import SET_NULL
+from django.forms import model_to_dict
 
 from medical.model_manager import PatientManager
 
 
-# Create your models here.
+
 class Patient(models.Model):
     """
     Model for the Patientrecord
     """
     name = models.CharField(max_length=200)
-    icd11Code = models.CharField(max_length=64, blank=True, default='')
-    diagnosisText = models.TextField(max_length=4096)
-    attendingDoctor = models.CharField(max_length=64,blank=True,default='')
-
-    roomNumber = models.CharField(max_length=64)
+    icd11_code = models.CharField(max_length=64, blank=True, default='')
+    diagnosis_text = models.TextField(max_length=4096)
+    attending_doctor = models.CharField(max_length=64,blank=True,default='')
+    room_number = models.IntegerField()
     is_related_to_staff = models.BooleanField(default=False)
 
     objects_patients = PatientManager()
 
     def __str__(self):
         """String for representing the Patient-Model object."""
-        return "{} withs pk-Number {}".format(
-            self.name, self.pk
-        )
-
-    # def save(self, *args, **kwargs):
-    #     """Save new or changed Patients"""
-    #     super().save(*args, **kwargs)
-    #
-    # def delete(self, using=None, keep_parents=False):
-    #     """Delete Patients"""
-    #     super().delete()
+        return model_to_dict(self)
